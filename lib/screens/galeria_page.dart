2942fce53fe3a1a085caa6f2ac8f8e17f93966ef
MICHEL DA SILVA ALVES <michel_s_alves@estudante.sesisenai.org.br>
2026-09-04 21:36:55 -0300

    aula 04.09

--- lib/screens/galeria_page.dart
+++ lib/screens/galeria_page.dart
@@ -9,6 +9,37 @@ class GaleriaPage extends StatefulWidget{
 }
 
 class _GaleriaPageState extends State<GaleriaPage>{
+  final List<Map<String, String>> _lugares= [
+
+    {
+      'nome':'Los caracles',
+      'local':'Andes, Chile',
+      'imagem': 'assets/images/Cordilheira.jpg',
+      'descrição':'Uma estrada inca cordilheira dos anedes com curvas e morros',
+    },
+    {
+      'nome':'aurora boreal',
+      'local':'Alasca, Estados Unidos',
+      'imagem': 'assets/images/Aurora Boreal.jpg',
+      'descrição':'Fairbanks, que fica bem embaixo do oval auroral',
+    },
+    {
+      'nome':'machu picchu',
+      'local':'Urubamba, Peru',
+      'imagem': 'assets/images/Machupitcho.jpg',
+      'descrição':'Localizada no topo de uma montanha no distrito de Machupicchu, na província de Urubamba, no Peru',
+    },
+    {
+      'nome':'Patagônia',
+      'local':'Chile',
+      'imagem': 'assets/images/Patagonia.jpg',
+      'descrição':'A Patagônia Chilena é uma vasta região no extremo sul do continente americano, famosa por seus glaciares, montanhas imponentes, fiordes e parques nacionais',
+    },
+
+  ];
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,23 +48,111 @@ class _GaleriaPageState extends State<GaleriaPage>{
         title: Text("Galeria das melhores fotos"),
       ),
       drawer: MenuDrawer(),
-      body: SingleChildScrollView(
-        child: Container(
-          margin: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1nOe54vuw8ysRYvMxy1eEfSEBEgEJn6a-qFnWUWKXn9PMfd_tDegl-OY&s=10'),
-              SizedBox(height: 20,),
-              Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZjHkHjhIzEp9T2oEwxkvLAkoS369-P78rTJneVuBpMztEftH13aGz5IAf&s=10'),
-              SizedBox(height: 20,),
-              Image.asset('assets/images/bolsolulapraia.jpg'),
-              SizedBox(height: 20,),
-              Image.asset('assets/images/bolsolula.jpg')
-            ],
-          ),
+      body: Padding(padding:
+       EdgeInsets.all(10),
+      child: GridView.builder(
+        itemCount: _lugares.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
         ),
+
+        itemBuilder: (context, index){
+          final lugar = _lugares[index];
+          return _buildCardDestino(lugar);
+        }
+
       )
+
+    ),
+
     );
   }
 
+  Widget _buildCardDestino(Map<String, String> lugar) {
+    final String nome = lugar['nome'] ?? '';
+    final String local = lugar['local'] ?? '';
+    final String imagem = lugar['imagem'] ?? '';
+
+
+    //Inkwell deixa um componente filho clicavel
+    // desenhando um efeito no memo.
+    return InkWell(
+
+      onTap: () => {},
+      child: Card(
+        elevation: 3,
+        //Aqui usamos Stack porque queremos colocar o nomeado
+        //lugar na frente daa imagem e não embaixo dela
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            //ClipRRect recorta um componente flho para que imagem
+            // fique com os cantos arredondados
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                imagem,
+                fit: BoxFit.cover,
+
+
+              ),
+            ),
+
+            // posição onde dentro do Sateck este elemento vai ficar
+            Positioned(
+              left: 0, right: 0, bottom: 0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.black.withValues(alpha: 0.55),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(16),
+                      bottomRight: Radius.circular(16),
+                    )
+
+                  ),
+
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10,
+                    vertical: 8
+
+                  ),
+
+                  child: Column(
+
+                      children: [
+                        Text(
+                            nome,
+                          style: TextStyle(
+                            color: Colors.white
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                            color: Colors.amber,
+                            size: 14,
+                            ),
+                            SizedBox(width: 2,),
+                            Expanded(
+                                child: Text(local,
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                ),
+                            ),
+                            ),
+                          ],
+                        )
+                      ],
+
+                  ),
+                )
+            ),
+          ],
+        ),
+      ),
+
+
+    );
+  }
 }
